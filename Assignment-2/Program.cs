cbba167d56e69555316396f66b8670608ae7d30a
MuqtadaAhmad <s1023075@uomustansiriyah.edu.iq>
2024-10-23 15:36:19 +0300

    new

---
+++ Assignment-2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("Honda","Civic");
+            Console.Write(car.brand);
+            Console.WriteLine(","+car.modelName);
+            Truck truck = new Truck("Ford","F-150");
+            Console.Write(truck.brand);
+            Console.WriteLine("," + truck.modelName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment-2/Truck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    internal class Truck:MotorVehide
+    {
+        public Truck(string oBrand, string oModelName)
+        {
+            brand = oBrand;
+            modelName = oModelName;
+        }
+    }
+}
